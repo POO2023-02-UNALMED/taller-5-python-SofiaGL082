0637d8d8d58aefc11533974ea4331a65ca8045f7
SofiaGL082 <sogiraldo@unal.edu.co>
2023-09-22 21:17:39 -0500

    :apple:

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -34,10 +34,13 @@ class Animal:
         return cls._totalAnimales
     
     def totalPorTipo(self):
-        return ("Mamiferos: ", Mamifero.cantidadMamiferos(), "\nAves: ", Ave.cantidadAves(), "\nReptiles: ", Reptil.cantidadReptiles(), "\nPeces: ", Pez.cantidadPeces(), "\nAnfibios: ", Anfibio.cantidadAnfibios(), sep="") 
+        mensaje = "Mamiferos: " + Mamifero.cantidadMamiferos() + "\nAves: " + Ave.cantidadAves() + "\nReptiles: " + Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() + "\nAnfibios: " + Anfibio.cantidadAnfibios()
+        return mensaje
 
     def toString(self):
         if zoo == null:
-            return ("Mi nombre es", self._nombre, ", tengo una edad de", self._edad, ", habito en", self._habitat, "y mi genero es", self._genero)
+            mensaje = "Mi nombre es" + self._nombre + ", tengo una edad de" + self._edad + ", habito en" + self._habitat + "y mi genero es" + self._genero
+            return mensaje
         else:
-            return ("Mi nombre es", self._nombre, ", tengo una edad de", self._edad, ", habito en", self._habitat, "y mi genero es", self._genero, ", la zona en la que me ubico es",self._zona, ", en el zoo", self._zoo)
+            mensaje = "Mi nombre es" + self._nombre + ", tengo una edad de" + self._edad, " + habito en" + self._habitat + "y mi genero es", self._genero, " + la zona en la que me ubico es" + self._zona + ", en el zoo" + self._zoo
+            return mensaje
